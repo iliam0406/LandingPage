cb3aa4d7a87e6cb48d2519945c9be44ea263486d
iliam0406 <iliam.carrion@gmail.com>
2020-05-23 17:13:53 -0400

    Primer trabajo de React, Landing Page con Bootstrap

---
+++ src/footer.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import PropType from 'prop-types';
+
+function Footer(props){
+
+return(
+
+<footer class="py-5 bg-dark">
+    <div class="container">
+      <p class="m-0 text-center text-white">Copyright &copy; Your Website 2019</p>
+    </div>   
+</footer>
+
+
+);
+}
+
+export default Footer;
